c39d455db50927a45dfb72bd8928d8cb48aa240c
Stephen Townsend <26904641+townsste@users.noreply.github.com>
2022-10-26 14:13:16 -0700

    Update mergesrt.sh
    added new merges

--- mergesrt.sh
+++ mergesrt.sh
@@ -27,7 +27,12 @@ mergesrt() {
 
     echo "File $VIDEO_FILE exists, start merging"
     MERGE_FILE=$FILE_NAME'.merge'
-    mkvmerge -o "$MERGE_FILE" -s !$LANG "$VIDEO_FILE" --language 0:$LANG "$SRT_FILE"
+    if [ "${TYPE,,}" == "sdh" ] || [ "${TYPE,,}" == "hi" ]; then 
+        mkvmerge -o "$MERGE_FILE" -s !$LANG "$VIDEO_FILE" --language 0:$LANG --track-name 0:$TYPE --hearing-impaired-flag "0:yes" "$SRT_FILE"
+    elif [ "${TYPE,,}" == "forc"* ]; then 
+        mkvmerge -o "$MERGE_FILE" -s !$LANG "$VIDEO_FILE" --language 0:$LANG --track-name 0:$TYPE --forced-display-flag "0:yes" "$SRT_FILE"
+    else
+        mkvmerge -o "$MERGE_FILE" -s !$LANG "$VIDEO_FILE" --language 0:$LANG --track-name 0:$LANG "$SRT_FILE"
     RESULT=$?
     if [ "$RESULT" -eq "0" ] || [ "$RESULT" -eq "1" ]; then
         RESULT=$([ "$RESULT" -eq "0" ] && echo "merge succeeded" || echo "merge completed with warnings")
