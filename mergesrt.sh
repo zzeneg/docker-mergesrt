0fbf8d5f88c1adfe3625909c2ed90ed7125c4161
Stephen Townsend <26904641+townsste@users.noreply.github.com>
2022-10-26 14:17:01 -0700

    Update mergesrt.sh
    forgot fi

--- mergesrt.sh
+++ mergesrt.sh
@@ -33,6 +33,7 @@ mergesrt() {
         mkvmerge -o "$MERGE_FILE" -s !$LANG "$VIDEO_FILE" --language 0:$LANG --track-name 0:$TYPE --forced-display-flag "0:yes" "$SRT_FILE"
     else
         mkvmerge -o "$MERGE_FILE" -s !$LANG "$VIDEO_FILE" --language 0:$LANG --track-name 0:$LANG "$SRT_FILE"
+    fi
     RESULT=$?
     if [ "$RESULT" -eq "0" ] || [ "$RESULT" -eq "1" ]; then
         RESULT=$([ "$RESULT" -eq "0" ] && echo "merge succeeded" || echo "merge completed with warnings")
